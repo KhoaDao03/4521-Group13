2aa45a6fe17de980089638052fc6629ff60d4545
chris-canty <chriscanty3@gmail.com>
2024-03-01 16:42:50 -0500

    Made a small change to a function name because it was annoying me

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def login():
     return render_template('login.html')
 
 @app.route('/register')
