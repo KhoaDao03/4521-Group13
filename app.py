724e77eadcd2cc7125f7ce5694b65393cb11a1ea
chris-canty <chriscanty3@gmail.com>
2024-03-22 01:09:10 -0400

    Added almost all pages based on the flowchart and linked them up. Each person should be responsible for their pages.

--- app.py
+++ app.py
@@ -10,5 +10,57 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/doctorhome')
+def doctorhome():
+    return render_template('doctorhome.html')
+
+@app.route('/doctorprofile')
+def doctorprofile():
+    return render_template('doctorprofile.html')
+
+@app.route('/patientspage')
+def patientspage():
+    return render_template('patientspage.html')
+
+@app.route('/doctorprescriptions')
+def doctorprescriptions():
+    return render_template('doctorprescriptions.html')
+
+@app.route('/doctormedicaldocs')
+def doctormedicaldocs():
+    return render_template('doctormedicaldocs.html')
+
+@app.route('/doctorappointments')
+def doctorappointments():
+    return render_template('doctorappointments.html')
+
+@app.route('/patienthome')
+def patienthome():
+    return render_template('patienthome.html')
+
+@app.route('/patientprofile')
+def patientprofile():
+    return render_template('patientprofile.html')
+
+@app.route('/patientmedicaldocs')
+def patientmedicaldocs():
+    return render_template('patientmedicaldocs.html')
+
+@app.route('/patientprescriptions')
+def patientprescriptions():
+    return render_template('patientprescriptions.html')
+
+@app.route('/patientappointments')
+def patientappointments():
+    return render_template('patientappointments.html')
+
+@app.route('/billing')
+def billing():
+    return render_template('billing.html')
+
+@app.route('/doctorsearch')
+def doctorsearch():
+    return render_template('doctorsearch.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
